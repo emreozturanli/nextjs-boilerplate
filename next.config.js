1e6a83017df1e38e38ea7e665bc6555e8046de11
Lütfullah ÇELENK <81390201+lutfullahcelenk@users.noreply.github.com>
2022-09-08 16:26:37 +0300

    tailwind and some dependencies are donwloaded

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true",
+});
+const nextConfig = {
+	reactStrictMode: true,
+	env: {
+		BASE_API_URL: "/api",
+	},
+};
+
+module.exports = withBundleAnalyzer(nextConfig);
